87c0303fb12c27afec3d0191c5284bee59042112
Andrew Childs <ac@glomerate.com>
2017-09-08 22:27:33 -0400

    Fix fatal exception, console.log the recorded path

--- screenshot.js
+++ screenshot.js
@@ -7,7 +7,7 @@ var nightmare = Nightmare();
 var url = process.argv[2];
 var width = process.argv[3] || 1400;
 var height = process.argv[4] || 860;
-var delayInMs = process.argv[5] 10000;
+var delayInMs = process.argv[5] || 10000;
 
 var formattedDate = new Date().toISOString().replace(/:/g, '-');
 var path = './png/' + formattedDate + '.png';
@@ -18,4 +18,6 @@ nightmare
   .wait(delayInMs)
   .screenshot(path)
   .end()
-  .then();
+  .then(function () {
+    console.log('Saved screenshot to ' + path);
+  });
